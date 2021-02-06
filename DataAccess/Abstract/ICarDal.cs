44f172d11955b3ee97965f9dac68cb15bbcea524
Emre Demir <emre2548@gmail.com>
2021-02-06 08:48:39 +0300

    Code skeleton

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        List<Car> GetAll(); // Ekleyince CAr Manager list<Car> hatası gitti
+        void GetById();
+        //void GetAll();
+        void Add();
+        void Update();
+        void Delete();
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Eklendi");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Silindi");
+        }
+
+        public void GetById()
+        {
+            Console.WriteLine("ID ile Getirildi");
+        }
+
+        public void GetAll()
+        {
+            Console.WriteLine("Hepsi Getirildi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Güncellendi");
+        }
+
+        List<Car> ICarDal.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
